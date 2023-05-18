c24e3d70bd521df384e791754f2b40009a6dd482
Jonathan Bijl <sannoayo@gmail.com>
2023-05-18 11:58:56 -0400

    Connected Database

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,11 @@
 // Iteration #1
+const mongoose = require('mongoose');
+const Movie = require('../models/Drone.model');
+
+mongoose.connect('mongodb://127.0.0.1:27017/lab-express-drones')
+.then(() => {
+  console.log('connected to database');
+})
+.catch((error) => {
+  console.log('error connecting to database');
+})
