70ab47774d3fafaeb0e965ac09ccf28103910b8c
Jonathan Bijl <sannoayo@gmail.com>
2023-05-18 13:37:01 -0400

    Done

--- routes/drones.js
+++ routes/drones.js
@@ -1,36 +1,66 @@
 const express = require('express');
 const router = express.Router();
+const Drone = require("../models/Drone.model");
 
 // require the Drone model here
-
-router.get('/drones', (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
+// List all drones
+router.get('/drones', async (req, res) => {
+  try {
+    const allDronesFromDb = await Drone.find();
+    res.render('drones/list.hbs', { drones: allDronesFromDb });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+// Render the create drone form
+router.get('/drones/create', (req, res) => {
+  res.render('drones/create-form.hbs');
 });
 
-router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+// Create a new drone
+router.post('/drones/create', async (req, res) => {
+  const { name, propellers, maxSpeed } = req.body;
+  try {
+    await Drone.create({ name, propellers, maxSpeed });
+    res.redirect('/drones');
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+// Render the edit form for a specific drone
+router.get('/drones/:id/edit', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const drone = await Drone.findById(id);
+    res.render('drones/edit-form.hbs', { drone });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
-  // Iteration #4: Update the drone
-  // ... your code here
+// Update a specific drone
+router.post('/drones/:id/edit', async (req, res) => {
+  const { id } = req.params;
+  const { name, propellers, maxSpeed } = req.body;
+  try {
+    await Drone.findByIdAndUpdate(id, { name, propellers, maxSpeed });
+    res.redirect('/drones');
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-router.post('/drones/:id/delete', (req, res, next) => {
-  // Iteration #5: Delete the drone
-  // ... your code here
+// Delete a specific drone
+router.post('/drones/:id/delete', async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Drone.findByIdAndDelete(id);
+    res.redirect('/drones');
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
